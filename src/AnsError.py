c9d17c98ea9f199ad5fea7a4f25673a23708275b
Jordi Martínez <michaeljordimartinez@gmail.com>
2025-02-16 23:23:44 -0600

    Función bisección, escala básica

--- src/AnsError.py
+++ src/AnsError.py
@@ -4,18 +4,36 @@ from src.Rango import Rango
 
 class AnsError:
     
-    def calc_Xr(self,Range: Rango):
-        return ((Range.X1 + Range.Xu) // 2)
+    def calc_Xr(self,Rango: Rango):
+        return ((Rango.X1 + Rango.Xu) / 2)
         
 
     def calc_f(self,funcion, X):
         return funcion(X)
-
-    def biseccion(self,funcion, Range: Rango,memo = {}):
-        while():
-            pass
+    
+    def calc_Es(self,n):
+        #return (0.5e(2-n))
+        return abs((0.5 * (10**(2-n))))
+    
+    #Ea se calcula con el valor de Xr y Xr anterior
+    def calc_Ea(self, Xr, XrA):
+        return abs(((Xr - XrA) * (100))/(Xr))
         
 
-    def valor_cercano():
-        pass
+    def biseccion(self,funcion, Rango,Es,Xra,contador = 1):
+        Xr = self.calc_Xr(Rango)
+        mult_funciones = funcion(Rango.X1) * funcion(Xr)
+        if(contador != 0):
+            Ea = self.calc_Ea(Xr,Xra)
+            if(Ea <= Es):
+                return Xr, contador        
+        if(mult_funciones) < 0:
+            Rango.Xu = Xr
+        elif (mult_funciones) > 0:
+            Rango.X1 = Xr
+       
+        return self.biseccion(funcion,Rango,Es,Xr,contador + 1)
+
+
+   
 
