a6062f7c3d7b07410069b20f1765f83fbd0e2639
Jordi Martínez <michaeljordimartinez@gmail.com>
2025-03-08 22:16:33 -0600

    Se completó el algoritmo, incluyendo pruebas unitarias, exitosas para el Método numérico FalsaPosicion

--- src/FalsaPosicion.py
+++ src/FalsaPosicion.py
@@ -14,7 +14,7 @@ class Rango:
 
     @limite_superior.setter
     def limite_superior(self, limite_superior):
-        self.__limite_superior
+        self.__limite_superior = limite_superior
 
     @limite_inferior.setter
     def limite_inferior(self, limite_inferior):
@@ -23,9 +23,6 @@ class Rango:
 
 class FalsaPosicion:
     
-    def calc_false_position(self, rango: Rango):
-        pass
-
     def calc_Es(self, n):
         return abs((0.5) * pow(10, 2 - n))
     
@@ -33,7 +30,27 @@ class FalsaPosicion:
         return abs(((Xr - XrA) * (100))/(Xr))
     
     def calc_Xr(self, rango: Rango, funcion):
-        return (rango.limite_superior) - ((funcion(rango.limite_superior) * (rango.limite_inferior - rango.limite_superior))/((funcion(rango.limite_inferior)) - (funcion(rango.limite_superior))))
+        return ((rango.limite_superior) - ((funcion(rango.limite_superior) * (rango.limite_inferior - rango.limite_superior))/((funcion(rango.limite_inferior)) - (funcion(rango.limite_superior)))))
     
+    def calc_false_position(self, funcion, rango: Rango,es, Xra = 0, contador = 1):
+        try:
+            Xr = self.calc_Xr(rango,funcion)
+            mult_funciones = funcion(rango.limite_inferior) * funcion(Xr)
+            if(contador > 1):
+                Ea = self.calc_Ea(Xr, Xra)
+                if(Ea <= es):
+                    return Xr, contador
+
+            if mult_funciones == 0:
+                return Xr, contador
+            elif mult_funciones > 0:
+                rango.limite_inferior = Xr
+            else:
+                rango.limite_superior = Xr
+
+            return self.calc_false_position(funcion, rango, es, Xr, contador +1)
+        except:
+            print(f"Error Posible: PILA -- ", contador)
+
 
         
--- tests/TestFalsaPosicion.py
+++ tests/TestFalsaPosicion.py
@@ -1,9 +1,23 @@
 import unittest
+import numpy as np
+from src.FalsaPosicion import Rango as Rango
+from src.FalsaPosicion import FalsaPosicion as ReglaFalsa
 
-from src.Rango import Rango 
-from src.FalsaPosicion import FalsaPosicion
 
 class TestFalsaPosicion(unittest.TestCase):
 
-    def testCalc_Xr(self):
+    def testRango(self):
         pass
+
+    def testCalc_Xr(self):
+        regla_falsa = ReglaFalsa()
+        es = regla_falsa.calc_Es(3)
+        self.assertEqual(0.05, es)
+        rango = Rango(0,1)
+        X_esperado, i_esperado = 0.383539686, 9
+        X_result, I_result = regla_falsa.calc_false_position((lambda x:np.e**(-5*x) - x**2),rango,es)
+        print(f"Resultado de Xr : {X_result} and Expexted: {X_esperado}")
+        self.assertEqual(i_esperado,I_result)
+        X_result =float( "{:.9f}".format(X_result))
+        self.assertEqual(X_esperado,X_result)
+
